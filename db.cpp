b7e6774262e2c37f296ce0a6d9321b23ffd62dfc
cycad <cycad@greencams.net>
2015-12-06 12:15:41 -0800

    Fixed variable name

--- db.cpp
+++ db.cpp
@@ -177,8 +177,8 @@ db_entrypoint(void *unused)
 		host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
 		clock_get_time(cclock, &mts);
 		mach_port_deallocate(mach_task_self(), cclock);
-		ts->tv_sec = mts.tv_sec;
-		ts->tv_nsec = mts->tv_nsec;
+		abstimeout->tv_sec = mts.tv_sec;
+		abstimeout->tv_nsec = mts->tv_nsec;
 #else
 		clock_gettime(CLOCK_REALTIME, &abstimeout);
 #endif
